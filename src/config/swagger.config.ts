2f35ed612e5bb3fdcdd2b66edc9f946c0cab95e6
Renan Amaral <renanalvarado123@gmail.com>
2026-09-23 11:09:43 -0300

    feat: update Swagger setup to remove port parameter and add redirect to documentation

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,7 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-export function setupSwagger(app: INestApplication, port: number): void {
+export function setupSwagger(app: INestApplication): void {
   const config = new DocumentBuilder()
     .setTitle('Despensa Inteligente API')
     .setDescription(
@@ -57,6 +57,4 @@ export function setupSwagger(app: INestApplication, port: number): void {
       persistAuthorization: true,
     },
   });
-
-  console.log(`Swagger: http://localhost:${port}/api/docs`);
 }
